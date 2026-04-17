1bbe9df30fc67275359f9a5b4b12db8fc8625114
inext68 <135109032+inext68@users.noreply.github.com>
2026-04-17 08:09:41 +0200

    1

--- setup.php
+++ setup.php
@@ -1,29 +1,23 @@
 <?php
 defined('GLPI_ROOT') or die('Sorry. You can\'t access this file directly.');
 
-use GlpiPlugin\Shellcmd\AssetTab;
+use GlpiPlugin\Shellcmd\Rights;
 
+/**
+ * Inizializzazione plugin
+ */
 function plugin_init_shellcmd(): void {
    global $PLUGIN_HOOKS;
 
    $PLUGIN_HOOKS['csrf_compliant']['shellcmd'] = true;
 
-   // TAB sugli asset
-   Plugin::registerClass(
-      AssetTab::class,
-      [
-         'addtabon' => [
-            'Computer',
-            'NetworkEquipment',
-            'Peripheral'
-         ]
-      ]
-   );
-
    // Caricamento lingua
    loadPluginLocale('shellcmd');
 }
 
+/**
+ * Versione plugin
+ */
 function plugin_version_shellcmd(): array {
    return [
       'name'           => 'Shell Commands',
@@ -40,10 +34,68 @@ function plugin_version_shellcmd(): array {
    ];
 }
 
+/**
+ * Verifica prerequisiti
+ */
 function plugin_shellcmd_check_prerequisites(): bool {
    return true;
 }
 
+/**
+ * Verifica configurazione
+ */
 function plugin_shellcmd_check_config(): bool {
+   return true;
+}
+
+/**
+ * INSTALLAZIONE PLUGIN
+ */
+function plugin_shellcmd_install(): bool {
+   global $DB;
+
+   // Diritti
+   Rights::install();
+
+   // Tabella comandi
+   $query = <<<SQL
+CREATE TABLE IF NOT EXISTS `glpi_plugin_shellcmd_commands` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` TEXT,
+  `pre_cmd` TEXT,
+  `command` TEXT NOT NULL,
+  `post_cmd` TEXT,
+  `allowed_itemtypes` TEXT NOT NULL,
+  `is_enabled` TINYINT(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_unicode_ci;
+SQL;
+
+   $DB->queryOrDie(
+      $query,
+      'shellcmd: unable to create commands table'
+   );
+
+   return true;
+}
+
+/**
+ * DISINSTALLAZIONE PLUGIN
+ */
+function plugin_shellcmd_uninstall(): bool {
+   global $DB;
+
+   // Diritti
+   Rights::uninstall();
+
+   // Rimozione tabelle
+   $DB->queryOrDie(
+      "DROP TABLE IF EXISTS `glpi_plugin_shellcmd_commands`",
+      'shellcmd: unable to drop commands table'
+   );
+
    return true;
 }
